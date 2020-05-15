b342ae1cc87c32e17406d38a65351e760427e4df
yannaungsoe <naungsoe.yan@ufinity.com>
2020-05-15 18:31:26 +0800

    added vue.js project

---
+++ sls/src/components/AppContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content">
+    <section>
+      <h2>Text Input</h2>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+      <cv-text-input placeholder="click to see browser behaviour on mobile device" />
+    </section>
+    <section>
+      <h2>Text Area</h2>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+      <cv-text-area placeholder="click to see browser behaviour on mobile device" />
+    </section>
+    <section>
+      <h2>Typography</h2>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppContent'
+}
+</script>
+
+<style>
+.content {
+  padding: 1.5rem;
+}
+
+.content section ~ section {
+  margin: 2rem 0;
+}
+</style>
---
+++ sls/src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <cv-header aria-label="Carbon header">
+    <cv-skip-to-content href="#main-content">
+      Skip to content
+    </cv-skip-to-content>
+    <cv-header-name href="javascript:void(0)" prefix="IBM">
+      [Platform]
+    </cv-header-name>
+    <template slot="header-global">
+      <cv-header-global-action
+        aria-label="Notifications"
+        aria-controls="notifications-panel"
+      >
+        <Notification20 />
+      </cv-header-global-action>
+      <cv-header-global-action 
+        aria-label="User avatar" 
+        aria-controls="user-panel"
+      >
+        <UserAvatar20 />
+      </cv-header-global-action>
+      <cv-header-global-action
+        aria-label="App switcher"
+        aria-controls="switcher-panel"
+      >
+        <AppSwitcher20 />
+      </cv-header-global-action>
+    </template>
+  </cv-header>
+</template>
+
+<script>
+import Notification20 from '@carbon/icons-vue/es/notification/20';
+import UserAvatar20 from '@carbon/icons-vue/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20';
+
+export default {
+  name: 'AppHeader',
+  components: {
+    Notification20,
+    UserAvatar20,
+    AppSwitcher20
+  }
+}
+</script>
+
+<style>
+</style>
