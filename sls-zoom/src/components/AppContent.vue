7467a22535d9e83ce180fb70b256d9d7fed90939
yannaungsoe <naungsoe.yan@ufinity.com>
2020-05-22 11:59:39 +0800

    added DnD prototype

---
+++ sls-zoom/src/components/AppContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <drag-drop-question />
+  </div>
+</template>
+
+<script>
+import DragDropQuestion from './DragDropQuestion.vue';
+
+export default {
+  name: 'AppContent',
+  components: {
+    DragDropQuestion
+  }
+}
+</script>
+
+<style>
+.content {
+  padding: 1.5rem;
+}
+</style>
---
+++ sls-zoom/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue/src/index';
+
+Vue.config.productionTip = false
+Vue.use(CarbonComponentsVue);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
